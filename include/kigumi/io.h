5f51edfa8812735f3d0c0d86a92611766fde40ca
Masashi Mizuno <29035331+unageek@users.noreply.github.com>
2023-01-17 11:18:40 +0900

    Use `vector` as buffer

--- include/kigumi/io.h
+++ include/kigumi/io.h
@@ -19,42 +19,42 @@ void my_read(std::istream& in, T& x) {
 
 template <>
 inline void my_write(std::ostream& out, const mpq_class& x) {
-  auto neg = static_cast<std::int8_t>(x.get_num() < 0);
+  auto neg = static_cast<std::uint8_t>(x.get_num() < 0);
   out.write(reinterpret_cast<const char*>(&neg), sizeof(neg));
 
   std::size_t count{};
-  auto* raw = mpz_export(nullptr, &count, 1, 1, 0, 0, x.get_num().get_mpz_t());
-  out.write(reinterpret_cast<const char*>(&count), sizeof(count));
-  out.write(reinterpret_cast<const char*>(raw), static_cast<std::streamsize>(count));
-  // NOLINTNEXTLINE(cppcoreguidelines-no-malloc, cppcoreguidelines-owning-memory)
-  std::free(raw);
+  std::vector<std::uint8_t> buf;
 
-  raw = mpz_export(nullptr, &count, 1, 1, 0, 0, x.get_den().get_mpz_t());
+  buf.resize((mpz_sizeinbase(x.get_num().get_mpz_t(), 2) + 7) / 8);
+  auto* raw = mpz_export(buf.data(), &count, 1, 1, 0, 0, x.get_num().get_mpz_t());
   out.write(reinterpret_cast<const char*>(&count), sizeof(count));
-  out.write(reinterpret_cast<const char*>(raw), static_cast<std::streamsize>(count));
-  // NOLINTNEXTLINE(cppcoreguidelines-no-malloc, cppcoreguidelines-owning-memory)
-  std::free(raw);
+  out.write(reinterpret_cast<const char*>(buf.data()), static_cast<std::streamsize>(count));
+
+  buf.resize((mpz_sizeinbase(x.get_den().get_mpz_t(), 2) + 7) / 8);
+  raw = mpz_export(buf.data(), &count, 1, 1, 0, 0, x.get_den().get_mpz_t());
+  out.write(reinterpret_cast<const char*>(&count), sizeof(count));
+  out.write(reinterpret_cast<const char*>(buf.data()), static_cast<std::streamsize>(count));
 }
 
 template <>
 inline void my_read(std::istream& in, mpq_class& x) {
+  std::uint8_t neg{};
+  in.read(reinterpret_cast<char*>(&neg), sizeof(neg));
+
   mpz_class num;
   mpz_class den;
   std::size_t count{};
-  std::vector<std::uint8_t> v;
-
-  std::int8_t neg{};
-  in.read(reinterpret_cast<char*>(&neg), sizeof(neg));
+  std::vector<std::uint8_t> buf;
 
   in.read(reinterpret_cast<char*>(&count), sizeof(count));
-  v.resize(count);
-  in.read(reinterpret_cast<char*>(v.data()), static_cast<std::streamsize>(count));
-  mpz_import(num.get_mpz_t(), count, 1, 1, 0, 0, v.data());
+  buf.resize(count);
+  in.read(reinterpret_cast<char*>(buf.data()), static_cast<std::streamsize>(count));
+  mpz_import(num.get_mpz_t(), count, 1, 1, 0, 0, buf.data());
 
   in.read(reinterpret_cast<char*>(&count), sizeof(count));
-  v.resize(count);
-  in.read(reinterpret_cast<char*>(v.data()), static_cast<std::streamsize>(count));
-  mpz_import(den.get_mpz_t(), count, 1, 1, 0, 0, v.data());
+  buf.resize(count);
+  in.read(reinterpret_cast<char*>(buf.data()), static_cast<std::streamsize>(count));
+  mpz_import(den.get_mpz_t(), count, 1, 1, 0, 0, buf.data());
 
   x = mpq_class(num) / mpq_class(den);
   if (static_cast<bool>(neg)) {
