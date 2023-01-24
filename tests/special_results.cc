4e5482876606855bb86f7d4c42a03f68530501dc
unageek <29035331+unageek@users.noreply.github.com>
2023-01-25 00:01:36 +0900

    More tests

--- tests/special_results.cc
+++ tests/special_results.cc
@@ -4,61 +4,131 @@
 #include "make_cube.h"
 
 using K = CGAL::Exact_predicates_exact_constructions_kernel;
-using kigumi::Operator;
 using Face_data = std::nullptr_t;
+using kigumi::Operator;
+
+auto mk_cube(double length, bool invert = false) {
+  auto min = -length / 2.0;
+  auto max = length / 2.0;
+  return make_cube<K, Face_data>({min, min, min}, {max, max, max}, nullptr, invert);
+}
 
 TEST(SpecialResultTest, A) {
-  auto m1 = make_cube<K, Face_data>({0, 0, 0}, {1, 1, 1}, nullptr, true);
-  auto m2 = make_cube<K, Face_data>({2, 0, 0}, {3, 1, 1}, nullptr, true);
-  auto m = m1.boolean(m2).apply(Operator::A);
-  ASSERT_TRUE(m.is_entire());
+  {
+    auto m1 = mk_cube(1, true);
+    auto m2 = mk_cube(2);
+    auto m = m1.boolean(m2).apply(Operator::A);
+    ASSERT_TRUE(m.is_entire());
+  }
+  {
+    auto m1 = mk_cube(1, true);
+    auto m2 = mk_cube(1);
+    auto m = m1.boolean(m2).apply(Operator::A);
+    ASSERT_TRUE(m.is_entire());
+  }
 }
 
 TEST(SpecialResultTest, B) {
-  auto m1 = make_cube<K, Face_data>({0, 0, 0}, {3, 3, 3}, nullptr);
-  auto m2 = make_cube<K, Face_data>({1, 1, 1}, {2, 2, 2}, nullptr);
-  auto m = m1.boolean(m2).apply(Operator::B);
-  ASSERT_TRUE(m.is_entire());
+  {
+    auto m1 = mk_cube(2);
+    auto m2 = mk_cube(1);
+    auto m = m1.boolean(m2).apply(Operator::B);
+    ASSERT_TRUE(m.is_entire());
+  }
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(1);
+    auto m = m1.boolean(m2).apply(Operator::B);
+    ASSERT_TRUE(m.is_entire());
+  }
 }
 
 TEST(SpecialResultTest, C) {
-  auto m1 = make_cube<K, Face_data>({1, 1, 1}, {2, 2, 2}, nullptr);
-  auto m2 = make_cube<K, Face_data>({0, 0, 0}, {3, 3, 3}, nullptr);
-  auto m = m1.boolean(m2).apply(Operator::C);
-  ASSERT_TRUE(m.is_entire());
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(2);
+    auto m = m1.boolean(m2).apply(Operator::C);
+    ASSERT_TRUE(m.is_entire());
+  }
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(1);
+    auto m = m1.boolean(m2).apply(Operator::C);
+    ASSERT_TRUE(m.is_entire());
+  }
 }
 
 TEST(SpecialResultTest, D) {
-  auto m1 = make_cube<K, Face_data>({0, 0, 0}, {1, 1, 1}, nullptr);
-  auto m2 = make_cube<K, Face_data>({2, 0, 0}, {3, 1, 1}, nullptr);
-  auto m = m1.boolean(m2).apply(Operator::D);
-  ASSERT_TRUE(m.is_entire());
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(2, true);
+    auto m = m1.boolean(m2).apply(Operator::D);
+    ASSERT_TRUE(m.is_entire());
+  }
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(1, true);
+    auto m = m1.boolean(m2).apply(Operator::D);
+    ASSERT_TRUE(m.is_entire());
+  }
 }
 
 TEST(SpecialResultTest, K) {
-  auto m1 = make_cube<K, Face_data>({0, 0, 0}, {1, 1, 1}, nullptr);
-  auto m2 = make_cube<K, Face_data>({2, 0, 0}, {3, 1, 1}, nullptr);
-  auto m = m1.boolean(m2).apply(Operator::K);
-  ASSERT_TRUE(m.is_empty());
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(2, true);
+    auto m = m1.boolean(m2).apply(Operator::K);
+    ASSERT_TRUE(m.is_empty());
+  }
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(1, true);
+    auto m = m1.boolean(m2).apply(Operator::K);
+    ASSERT_TRUE(m.is_empty());
+  }
 }
 
 TEST(SpecialResultTest, L) {
-  auto m1 = make_cube<K, Face_data>({1, 1, 1}, {2, 2, 2}, nullptr);
-  auto m2 = make_cube<K, Face_data>({0, 0, 0}, {3, 3, 3}, nullptr);
-  auto m = m1.boolean(m2).apply(Operator::L);
-  ASSERT_TRUE(m.is_empty());
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(2);
+    auto m = m1.boolean(m2).apply(Operator::L);
+    ASSERT_TRUE(m.is_empty());
+  }
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(1);
+    auto m = m1.boolean(m2).apply(Operator::L);
+    ASSERT_TRUE(m.is_empty());
+  }
 }
 
 TEST(SpecialResultTest, M) {
-  auto m1 = make_cube<K, Face_data>({0, 0, 0}, {3, 3, 3}, nullptr);
-  auto m2 = make_cube<K, Face_data>({1, 1, 1}, {2, 2, 2}, nullptr);
-  auto m = m1.boolean(m2).apply(Operator::M);
-  ASSERT_TRUE(m.is_empty());
+  {
+    auto m1 = mk_cube(2);
+    auto m2 = mk_cube(1);
+    auto m = m1.boolean(m2).apply(Operator::M);
+    ASSERT_TRUE(m.is_empty());
+  }
+  {
+    auto m1 = mk_cube(1);
+    auto m2 = mk_cube(1);
+    auto m = m1.boolean(m2).apply(Operator::M);
+    ASSERT_TRUE(m.is_empty());
+  }
 }
 
 TEST(SpecialResultTest, X) {
-  auto m1 = make_cube<K, Face_data>({0, 0, 0}, {1, 1, 1}, nullptr, true);
-  auto m2 = make_cube<K, Face_data>({2, 0, 0}, {3, 1, 1}, nullptr, true);
-  auto m = m1.boolean(m2).apply(Operator::X);
-  ASSERT_TRUE(m.is_empty());
+  {
+    auto m1 = mk_cube(1, true);
+    auto m2 = mk_cube(2);
+    auto m = m1.boolean(m2).apply(Operator::X);
+    ASSERT_TRUE(m.is_empty());
+  }
+  {
+    auto m1 = mk_cube(1, true);
+    auto m2 = mk_cube(1);
+    auto m = m1.boolean(m2).apply(Operator::X);
+    ASSERT_TRUE(m.is_empty());
+  }
 }
