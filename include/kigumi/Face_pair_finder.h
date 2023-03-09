29f77b8c7df6fdc7efa060ac9f47c2ef0e07c12e
Masashi Mizuno <29035331+unageek@users.noreply.github.com>
2023-03-09 13:48:20 +0900

    Do not corefine coincidental faces

--- include/kigumi/Face_pair_finder.h
+++ include/kigumi/Face_pair_finder.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <kigumi/AABB_tree/Coincide.h>
 #include <kigumi/AABB_tree/Overlap.h>
 #include <kigumi/Triangle_soup.h>
 
 #include <iterator>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -20,46 +22,124 @@ class Face_pair_finder {
       : left_{left}, right_{right} {}
 
   std::vector<Face_pair> find_face_pairs() const {
-    std::vector<Face_pair> pairs;
+    std::unordered_set<Face_handle> left_fhs_to_skip;
+    std::unordered_set<Face_handle> right_fhs_to_skip;
+    {
+      std::vector<Face_pair> pairs;
 
 #pragma omp parallel
-    {
-      std::vector<Face_pair> local_pairs;
-      std::vector<const Leaf*> leaves;
+      {
+        std::vector<Face_pair> local_pairs;
+        std::vector<const Leaf*> leaves;
 
-      if (left_.num_faces() < right_.num_faces()) {
-        const auto& left_tree = left_.aabb_tree();
-
-#pragma omp for schedule(guided)
-        for (std::ptrdiff_t i = 0; i < static_cast<std::ptrdiff_t>(right_.num_faces()); ++i) {
-          Face_handle fh{static_cast<std::size_t>(i)};
-          leaves.clear();
-          left_tree.template get_intersecting_leaves<Overlap<K>>(std::back_inserter(leaves),
-                                                                 right_.triangle(fh));
-          for (auto leaf : leaves) {
-            local_pairs.emplace_back(leaf->face_handle(), fh);
-          }
-        }
-      } else {
-        const auto& right_tree = right_.aabb_tree();
+        if (left_.num_faces() < right_.num_faces()) {
+          const auto& left_tree = left_.aabb_tree();
 
 #pragma omp for schedule(guided)
-        for (std::ptrdiff_t i = 0; i < static_cast<std::ptrdiff_t>(left_.num_faces()); ++i) {
-          Face_handle fh{static_cast<std::size_t>(i)};
-          leaves.clear();
-          right_tree.template get_intersecting_leaves<Overlap<K>>(std::back_inserter(leaves),
-                                                                  left_.triangle(fh));
-          for (auto leaf : leaves) {
-            local_pairs.emplace_back(fh, leaf->face_handle());
+          for (std::ptrdiff_t i = 0; i < static_cast<std::ptrdiff_t>(right_.num_faces()); ++i) {
+            Face_handle fh{static_cast<std::size_t>(i)};
+            auto right_tri = right_.triangle(fh);
+            leaves.clear();
+            left_tree.template get_intersecting_leaves<Coincide<K>>(std::back_inserter(leaves),
+                                                                    right_.triangle(fh));
+            for (const auto* leaf : leaves) {
+              auto left_tri = left_.triangle(leaf->face_handle());
+              if (left_tri[0] == right_tri[0] &&
+                  ((left_tri[1] == right_tri[1] && left_tri[2] == right_tri[2]) ||
+                   (left_tri[1] == right_tri[2] && left_tri[2] == right_tri[1]))) {
+                local_pairs.emplace_back(leaf->face_handle(), fh);
+              }
+            }
+          }
+        } else {
+          const auto& right_tree = right_.aabb_tree();
+
+#pragma omp for schedule(guided)
+          for (std::ptrdiff_t i = 0; i < static_cast<std::ptrdiff_t>(left_.num_faces()); ++i) {
+            Face_handle fh{static_cast<std::size_t>(i)};
+            auto left_tri = left_.triangle(fh);
+            leaves.clear();
+            right_tree.template get_intersecting_leaves<Coincide<K>>(std::back_inserter(leaves),
+                                                                     left_.triangle(fh));
+            for (const auto* leaf : leaves) {
+              auto right_tri = right_.triangle(leaf->face_handle());
+              if (left_tri[0] == right_tri[0] &&
+                  ((left_tri[1] == right_tri[1] && left_tri[2] == right_tri[2]) ||
+                   (left_tri[1] == right_tri[2] && left_tri[2] == right_tri[1]))) {
+                local_pairs.emplace_back(fh, leaf->face_handle());
+              }
+            }
           }
         }
-      }
 
 #pragma omp critical
-      pairs.insert(pairs.end(), local_pairs.begin(), local_pairs.end());
+        pairs.insert(pairs.end(), local_pairs.begin(), local_pairs.end());
+      }
+
+      for (const auto& [left_fh, right_fh] : pairs) {
+        left_fhs_to_skip.insert(left_fh);
+        right_fhs_to_skip.insert(right_fh);
+      }
     }
 
-    return pairs;
+    {
+      std::vector<Face_pair> pairs;
+
+#pragma omp parallel
+      {
+        std::vector<Face_pair> local_pairs;
+        std::vector<const Leaf*> leaves;
+
+        if (left_.num_faces() < right_.num_faces()) {
+          const auto& left_tree = left_.aabb_tree();
+
+#pragma omp for schedule(guided)
+          for (std::ptrdiff_t i = 0; i < static_cast<std::ptrdiff_t>(right_.num_faces()); ++i) {
+            Face_handle fh{static_cast<std::size_t>(i)};
+            if (right_fhs_to_skip.contains(fh)) {
+              continue;
+            }
+
+            leaves.clear();
+            left_tree.template get_intersecting_leaves<Overlap<K>>(std::back_inserter(leaves),
+                                                                   right_.triangle(fh));
+            for (const auto* leaf : leaves) {
+              if (left_fhs_to_skip.contains(leaf->face_handle())) {
+                continue;
+              }
+
+              local_pairs.emplace_back(leaf->face_handle(), fh);
+            }
+          }
+        } else {
+          const auto& right_tree = right_.aabb_tree();
+
+#pragma omp for schedule(guided)
+          for (std::ptrdiff_t i = 0; i < static_cast<std::ptrdiff_t>(left_.num_faces()); ++i) {
+            Face_handle fh{static_cast<std::size_t>(i)};
+            if (left_fhs_to_skip.contains(fh)) {
+              continue;
+            }
+
+            leaves.clear();
+            right_tree.template get_intersecting_leaves<Overlap<K>>(std::back_inserter(leaves),
+                                                                    left_.triangle(fh));
+            for (const auto* leaf : leaves) {
+              if (right_fhs_to_skip.contains(leaf->face_handle())) {
+                continue;
+              }
+
+              local_pairs.emplace_back(fh, leaf->face_handle());
+            }
+          }
+        }
+
+#pragma omp critical
+        pairs.insert(pairs.end(), local_pairs.begin(), local_pairs.end());
+      }
+
+      return pairs;
+    }
   }
 
  private:
