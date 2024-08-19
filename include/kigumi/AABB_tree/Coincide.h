5d419ff79af8afe3673c9b7629233691b9cdd9fb
unageek <29035331+unageek@users.noreply.github.com>
2024-08-20 06:01:21 +0900

    Fix namespace

--- include/kigumi/AABB_tree/Coincide.h
+++ include/kigumi/AABB_tree/Coincide.h
@@ -2,14 +2,14 @@
 
 #include <CGAL/Bbox_3.h>
 
+namespace kigumi {
+
 template <class K>
 class Coincide {
   using Bbox = CGAL::Bbox_3;
 
  public:
-  static bool do_intersect(const Bbox& bbox, const Bbox& query_bbox) {
-    return bbox.xmin() <= query_bbox.xmin() && bbox.xmax() >= query_bbox.xmax() &&
-           bbox.ymin() <= query_bbox.ymin() && bbox.ymax() >= query_bbox.ymax() &&
-           bbox.zmin() <= query_bbox.zmin() && bbox.zmax() >= query_bbox.zmax();
-  }
+  static bool do_intersect(const Bbox& bbox, const Bbox& query_bbox) { return bbox == query_bbox; }
 };
+
+}  // namespace kigumi
