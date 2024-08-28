70b4e3dee5a5b87417abc4a7f0577d22d47dd586
unageek <29035331+unageek@users.noreply.github.com>
2024-08-28 18:16:20 +0900

    Optimize insertion to triangulation

--- include/kigumi/Triangulator.h
+++ include/kigumi/Triangulator.h
@@ -7,6 +7,8 @@
 #include <kigumi/Triangle_region.h>
 
 #include <array>
+#include <stdexcept>
+#include <unordered_map>
 
 namespace kigumi {
 
@@ -28,9 +30,13 @@ class Triangulator {
   Triangulator(Triangle_region f, const Point& pa, const Point& pb, const Point& pc, std::size_t a,
                std::size_t b, std::size_t c)
       : f_{f}, cdt_{make_cdt_traits(pa, pb, pc)} {
-    vhs_[0] = insert(pa, a);
-    vhs_[1] = insert(pb, b);
-    vhs_[2] = insert(pc, c);
+    // To keep id_to_vh_ small, we do not insert these vertices into it.
+    vhs_[0] = cdt_.insert_outside_affine_hull(pa);
+    vhs_[0]->info() = a;
+    vhs_[1] = cdt_.insert_outside_affine_hull(pb);
+    vhs_[1]->info() = b;
+    vhs_[2] = cdt_.insert_outside_affine_hull(pc);
+    vhs_[2]->info() = c;
   }
 
   template <class OutputIterator>
@@ -43,34 +49,54 @@ class Triangulator {
     }
   }
 
-  Vertex_handle insert(const Point& p, std::size_t id) {
-    auto vh = cdt_.insert(p);
-    vh->info() = id;
-    return vh;
-  }
-
   Vertex_handle insert(const Point& p, std::size_t id, Triangle_region region) {
-    Vertex_handle vh;
-    region = intersection(region, f_);
-    if (region == Triangle_region::LeftEdge01 || region == Triangle_region::RightEdge01) {
-      vh = insert_in_edge(p, id, 0);
-    } else if (region == Triangle_region::LeftEdge12 || region == Triangle_region::RightEdge12) {
-      vh = insert_in_edge(p, id, 1);
-    } else if (region == Triangle_region::LeftEdge20 || region == Triangle_region::RightEdge20) {
-      vh = insert_in_edge(p, id, 2);
-    } else {
-      vh = cdt_.insert(p);
+    switch (intersection(region, f_)) {
+      case Triangle_region::LeftVertex0:
+      case Triangle_region::RightVertex0:
+        return vhs_[0];
+
+      case Triangle_region::LeftVertex1:
+      case Triangle_region::RightVertex1:
+        return vhs_[1];
+
+      case Triangle_region::LeftVertex2:
+      case Triangle_region::RightVertex2:
+        return vhs_[2];
+
+      case Triangle_region::LeftEdge01:
+      case Triangle_region::RightEdge01:
+        return insert_in_edge(p, id, 0);
+
+      case Triangle_region::LeftEdge12:
+      case Triangle_region::RightEdge12:
+        return insert_in_edge(p, id, 1);
+
+      case Triangle_region::LeftEdge20:
+      case Triangle_region::RightEdge20:
+        return insert_in_edge(p, id, 2);
+
+      case Triangle_region::LeftFace:
+      case Triangle_region::RightFace:
+        return insert_in_face(p, id);
+
+      default:
+        throw std::runtime_error("invalid region");
     }
-    vh->info() = id;
-    return vh;
   }
 
   void insert_constraint(Vertex_handle vh_i, Vertex_handle vh_j) {
     cdt_.insert_constraint(vh_i, vh_j);
   }
 
+  Triangle_region triangle_region() const { return f_; }
+
  private:
   Vertex_handle insert_in_edge(const Point& p, std::size_t id, int ei) {
+    auto it = id_to_vh_.find(id);
+    if (it != id_to_vh_.end()) {
+      return it->second;
+    }
+
     Vertex_handle vh;
     Face_handle fh;
     if (cdt_.is_edge(vhs_.at(ei), vhs_.at((ei + 1) % 3), fh, ei)) {
@@ -79,6 +105,25 @@ class Triangulator {
       vh = cdt_.insert(p);
     }
     vh->info() = id;
+    id_to_vh_.emplace(id, vh);
+    return vh;
+  }
+
+  Vertex_handle insert_in_face(const Point& p, std::size_t id) {
+    auto it = id_to_vh_.find(id);
+    if (it != id_to_vh_.end()) {
+      return it->second;
+    }
+
+    Vertex_handle vh;
+    Face_handle fh;
+    if (cdt_.is_face(vhs_[0], vhs_[1], vhs_[2], fh)) {
+      vh = cdt_.insert(p, CDT::FACE, fh, -1);
+    } else {
+      vh = cdt_.insert(p);
+    }
+    vh->info() = id;
+    id_to_vh_.emplace(id, vh);
     return vh;
   }
 
@@ -89,6 +134,7 @@ class Triangulator {
   Triangle_region f_{};
   CDT cdt_;
   std::array<Vertex_handle, 3> vhs_;
+  std::unordered_map<std::size_t, Vertex_handle> id_to_vh_;
 };
 
 }  // namespace kigumi
